1fce3425086742615e8084b95f297fb22154bb36
Nikolai <kolechka.malyshev.04@gmail.com>
2023-10-06 13:26:20 +0700

    start write user commands

--- SearchSubstring.CMD/Program.cs
+++ SearchSubstring.CMD/Program.cs
@@ -7,14 +7,36 @@ namespace SearchSubstring.CMD
     {
         static void Main(string[] args)
         {
-            SearcherController searcherController = new(char.ConvertToUtf32("A".ToString(), 0), char.ConvertToUtf32("z".ToString(), 0));
-            Highlight(searcherController);
+            while (true)
+            {
+                SearcherController searcherController = new(char.ConvertToUtf32("A".ToString(), 0), char.ConvertToUtf32("z".ToString(), 0));
+                Console.WriteLine("Введите одну из следующих команд (или END):\n" +
+                                  "1 - Поиск подстроки в файле и вывод с выделением\n" +
+                                  "2 - Поиск подстроки в строке с выделением\n" +
+                                  "3 - Поиск подстроки в строке без выделения");
+                string? command = Console.ReadLine();
+                switch (command)
+                {
+                    case "1": 
+                        OpenFile();
+                        break;
+                    case "2":
+                        Console.WriteLine("Введите строку, в которой будет проводиться поиск: ");
+                        string? currentString = Console.ReadLine();
+                        string[] currentSubstrings = getSubstrings();
+                        if (currentString != null)
+                        {
+                            Highlight(searcherController, currentString, currentSubstrings);
+                        }
+                        break;
+                }
+            }
         }
 
-        public static void Highlight(SearcherController searcherController)
+        public static void Highlight(SearcherController searcherController, string currentString, string[] currentSubstrings)
         {
             Dictionary<string, List<int>> dictionary =
-                searcherController.Search("aAbCbccaabc", new String[2] { "abc", "bc" }, caseSensitivity: false, method: "last", count: 1);
+                searcherController.Search(currentString, currentSubstrings, caseSensitivity: false, method: "last", count: 1);
 
             int i = 0;
             int indexEndOfPaint = 0;
@@ -69,6 +91,25 @@ namespace SearchSubstring.CMD
             Console.WriteLine("Введите полный путь до файла(.txt): ");
             string filePath = Console.ReadLine();
             return "";
+        }
+
+        public static string[] getSubstrings()
+        {
+            Console.WriteLine("Введите количество подстрок, которое вы хотите искать: ");
+            int countSubstrings = int.Parse(Console.ReadLine());
+            string[] currentSubstrings = new string[countSubstrings];
+            for (int i = 0; i < countSubstrings; i++)
+            {
+                Console.WriteLine(string.Concat("Введите подстроку #", i + 1, ": "));
+                currentSubstrings[i] = Console.ReadLine();
+            }
+            return currentSubstrings;
+        }
+
+        public static Dictionary<string, string> getFlags()
+        {
+            Console.WriteLine("Выберите одну из команд: ");
+            return new Dictionary<string, string>();
         }
     }
 }
